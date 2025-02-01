b13b870ebc22db2a209f1167e93c2614e5d45dd1
Salil Srivastava <srivastavasalil2211@gmail.com>
2025-02-01 12:23:05 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let head = document.querySelector('.head');
+let image = document.querySelector('.image');
+let sapna = document.querySelector('.sapna');
+const btn = document.querySelector('.btnn');
+console.log("hello");
+function meme(){
+    fetch('https://meme-api.com/gimme/wholesomememes')
+    .then((res)=>res.json())
+    .then((data)=>{
+       const{author,title,url} = data
+       console.log(url);
+      head.innerHTML = `${title} `
+      image.src= url
+      sapna.innerHTML = `Made By: ${author}`
+    })
+}
+
+meme()
+btn.addEventListener('click', ()=>{
+    meme()
+})
+
